ef807cdacfa346c199424f2b5f663c2df9524efb
saboodev <sabrina_desantis@hotmail.fr>
2021-12-22 02:53:01 +0100

    essai ajout du LS

--- front/js/product.js
+++ front/js/product.js
@@ -9,6 +9,7 @@ let productDescription = document.querySelector("#description");
 let productPrice = document.querySelector("#price");
 let productSelect = document.querySelector("#colors");
 
+// On appelle l'id de la page
 getProduct();
 
 function getProduct() {
@@ -24,7 +25,7 @@ function getProduct() {
         // Organiser les données des produits
         .then(function (apiResponse) {
             let canape = apiResponse;
-            console.log(canape);
+            // console.log(canape);
             productImg.src = canape.imageUrl;
             productTitle.innerHTML = canape.name;
             productPrice.innerHTML = canape.price;
@@ -33,7 +34,7 @@ function getProduct() {
             let choiceColor = canape.colors;
             for (let i = 0; i < choiceColor.length; i++) {
                 let option = choiceColor[i];
-                console.log(option);
+                // console.log(option);
                 productColor = document.createElement("option");
                 productColor.textContent = option;
                 productColor.value = option;
@@ -41,5 +42,57 @@ function getProduct() {
             }  
         })
         .catch((err) => console.log('Erreur : ' +err));
+};
+
+// *************** Gestion du localStorage *************
+
+add ();
+
+// Créatoion fonction d'ajout de produit
+function add() {
+    const addToCart = document.querySelector("#addToCart");
+    const productQuantity = document.querySelector("#itemQuantity");
+    let cart = {
+        addedQuantity: productChoosen.value,
+        color: productChoosen.option,
+        _id: id,
+    };
+
+    // Ecouter le bouton d'envoi et envoyer le panier
+    addToCart.addEventListener("click", (e) => {
+        e.preventDefault();
+        let addProduct = JSON.parse(localStorage.getItem("cart"));
+
+        // On récupère les valeurs, si quantité > 0
+        if (productQuantity > 0 && productQuantity < 100) {
+            addToCart.innerText = `Ajouté !`;
+            
+            // Si produit déjà enregistré dans le LS, on ajoute le produit 
+            if (addProduct) {
+                addProduct.push(cart);
+                localStorage.setItem("cart", JSON.stringify(addProduct));
+                confirmation();  
+            // Si pas de produit dans localStorage, on le crée          
+            }else {
+                addProduct = [];
+                addProduct.push(cart);
+                localStorage.setItem("cart", JSON.stringify(addProduct));
+                console.log(addProduct);
+                confirmation ();
+            }     
+        }else {
+            alert(`La valeur doit être comprise entre 1 et 100`);
+        } 
+        
+        // Fonction confirmation quand produit ajouté au panier
+        const confirmation = () => {
+            if (window.confirm(`Votre produit a bien été ajouté au panier
+    Consulter le panier OK ou Poursuivre vos achats Annuler`))
+            {
+                window.location.href = "cart.html";
+            }
         }
     
+    });
+}
+
